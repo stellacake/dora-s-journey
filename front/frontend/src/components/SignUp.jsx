a6d22fff886c7024f4b7a5df7b37f0aed5705df3
Stella <stella.dmt@gmail.com>
2020-12-14 23:56:58 +0100

    created registration form

--- front/frontend/src/components/SignUp.jsx
+++ front/frontend/src/components/SignUp.jsx
@@ -3,21 +3,56 @@ import React, { Component } from "react";
 class SignUp extends Component {
 	constructor() {
 		super();
-		this.state = { email: "coucou" };
-		this.updateEmailField = this.updateEmailField.bind(this);
+		this.state = {
+			firstname: "",
+			lastname: "",
+			email: "",
+			password: "",
+			passwordConf: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	updateEmailField(e) {
-		this.setState((state) => ({ email: e.target.value }));
+	handleChange(e) {
+		this.setState((state) => ({ [e.target.name]: e.target.value }));
 	}
+	handleSubmit(e) {
+		e.preventdefault();
+		console.log(this.state);
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>{this.state.email}</h1>
-				<input
-					type="email"
-					name="email"
-					onChange={(e) => this.updateEmailField(e)}
-				/>
+				<h1>{JSON.stringify(this.state, 1, 1)}</h1>
+				<form onSubmit={(e) => this.handleSubmit(e)}>
+					<input
+						type="firstname"
+						name="firstname"
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input
+						type="lastname"
+						name="lastname"
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input
+						type="email"
+						name="email"
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input
+						type="password"
+						name="password"
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input
+						type="passwordConf"
+						name="passwordConf"
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input type="submit" value="Submit" />
+				</form>
 			</div>
 		);
 	}
